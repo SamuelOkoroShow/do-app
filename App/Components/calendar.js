1f2ea14d2d45a1883869a2bf635268825df1adf9
Sam O <sam@samokoro.com>
2016-02-06 22:09:37 -0500

    Calendar Month State

--- App/Components/calendar.js
+++ App/Components/calendar.js
@@ -22,14 +22,24 @@ var customDayHeadings = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 class Walk extends Component {
     constructor() {
         super();
+        this.state ={
+         date: "February"
+        };
     }
   render() {
     
 return (
-     <View style={styles.container}><Calendar
+     <View style={styles.container}>
+    <View style={styles.row1}>
+    <Image source={require('image!menu')} resizeMode={'contain'} style={styles.menu}/>
+<Text style={{margin:15, fontSize:15,marginRight:0}}>{this.state.date}</Text>
+<Image source={require('image!down')} resizeMode={'contain'} style={{margin:15, width:10}}/>
+    </View>
+ <View style={styles.row2}>
+    <Calendar
           ref="calendar"
          
-          showControls={true}
+          showControls={false}
           dayHeadings={customDayHeadings}
           titleFormat={'MMMM YYYY'}
          
@@ -41,8 +51,16 @@ return (
               dayCircleFiller:{ borderRadius:0, padding:0, height:45, width:45}, weekRow:{backgroundColor:'#6664A5',flex:1, padding:0 }, selectedDayCircle:{backgroundColor:'#52CFC7', padding:0},
                   calendarHeading:{backgroundColor:'#6664A5', borderBottomWidth:1,borderTopWidth:0, borderBottomColor:'#6D6BAC', padding:7, }, 
                       currentDayCircle:{ backgroundColor:'#52CFC7'}, 
-              currentDayText:{padding:0,color:'#fff', fontSize:15, fontWeight:'600'},eventIndicatorFiller:{backgroundColor:'#fff', height:0}, dayButton:{borderBottomWidth:0, borderTopWidth:1,borderTopColor:'#6D6BAC', padding:0, margin:0}, dayHeading:{color:'#fff', fontSize:13}}}  
-          /></View>
+              currentDayText:{padding:0,color:'#fff', fontSize:15, fontWeight:'600'},eventIndicatorFiller:{backgroundColor:'#fff', height:0}, dayButton:{borderBottomWidth:0, borderTopWidth:1,borderTopColor:'#6D6BAC', padding:0, margin:0}, dayHeading:{color:'#fff', fontSize:13},title:{height:0},calendarContainer:{backgroundColor:'#fff'}}}  
+          />
+              </View>
+           <View style={styles.row3}>
+               <View style={styles.row1}></View>
+               <View style={styles.row1}></View>
+               <View style={styles.row1}></View>
+               </View>
+              
+              </View>
     );
  
 
@@ -58,6 +76,7 @@ const styles = StyleSheet.create({
     walk:{
     width:300
     },
+    
    
     close: {
         width: 20,
@@ -92,7 +111,26 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
-  }
+  }, menu: {
+        width: 21,
+        
+        margin:10,
+      marginBottom:0
+
+    },
+    row1:{
+    flex:1,
+        flexDirection:'row'
+        
+    },
+    row2:{
+    flex:6,
+        marginTop:-20,
+     
+    },
+    row3:{
+    flex:5
+    },
     }
 );
 module.exports = Walk;
