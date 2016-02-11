0ef69dbc1f3394d9d8d789d11729547aff44dbdc
Sam O <sam@samokoro.com>
2016-02-11 15:59:32 -0500

    Cleaner image & View

--- App/Components/profile.js
+++ App/Components/profile.js
@@ -13,16 +13,27 @@ from 'react-native';
 var Nav = require("./Helpers/nav");
 var TaskStats = require("./Helpers/taskStats");
 var Icon = require('react-native-vector-icons/Ionicons');
+var profileImg = require('image!profile');
 
 class Profile extends Component {
     constructor() {
         super();
     }
     
+    badge(){
+    return(
+    <View style={styles.badge}>
+        <Image source = {profileImg} style={{height:70, margin:10}} resizeMode='contain' />
+        <Text style={{color:'#222', margin:5, fontSize:22, fontWeight:'300'}}>Good Job Marie!</Text>
+        <Text style={{color:'#999', margin:5}}>You havent missed any tasks this week!</Text>
+        </View>
+    );
+    }
     render(){
     return(
         <View style={styles.container}>
         <Nav title ={{val:"Profile"}} />
+        {this.badge()}
         <TaskStats />
         
         </View>
@@ -33,6 +44,11 @@ class Profile extends Component {
     const styles = StyleSheet.create({
         container: {
             flex: 1,
+        },
+        badge:{
+        height:180,
+            alignItems:'center',
+            justifyContent:'center'
         }
 
     });
